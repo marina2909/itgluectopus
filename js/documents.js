c7c54382c4cd08dcc6cbc516b322004408b1eb1d
Marina Sovic <msovic@itglue.com>
2017-09-29 10:54:38 -0700

    Add Sarah's cool pictures

--- js/documents.js
+++ js/documents.js
@@ -1,5 +1,6 @@
-var srcDoc1 = "imgs/doc1.png";
-
+var srcDoc1 = "imgs/document.png";
+var srcDoc2 = "imgs/folder.png";
+var srcDoc3 = "imgs/lock.png";
 class Documents {
     constructor() {
         this.generate();
@@ -8,13 +9,17 @@ class Documents {
     generate () {
         this.documents = [];
         let sprite1 = new Sprite(srcDoc1, 1 * app.size, 1 * app.size, app.size, app.size);
-        let sprite2 = new Sprite(srcDoc1, 5 * app.size, 8 * app.size, app.size, app.size);
-        let sprite3 = new Sprite(srcDoc1, 8 * app.size, 9 * app.size, app.size, app.size);
+        let sprite2 = new Sprite(srcDoc2, 5 * app.size, 8 * app.size, app.size, app.size);
+        let sprite3 = new Sprite(srcDoc3, 8 * app.size, 9 * app.size, app.size, app.size);
         let sprite4 = new Sprite(srcDoc1, 4 * app.size, 4 * app.size, app.size, app.size);
+        let sprite5 = new Sprite(srcDoc2, 1 * app.size, 9 * app.size, app.size, app.size);
+        let sprite6 = new Sprite(srcDoc3, 9 * app.size, 1 * app.size, app.size, app.size);
         this.documents.push(sprite1);
         this.documents.push(sprite2);
         this.documents.push(sprite3);
         this.documents.push(sprite4);
+        this.documents.push(sprite5);
+        this.documents.push(sprite6);
     }
 
     draw () {
