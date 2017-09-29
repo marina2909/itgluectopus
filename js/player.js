5cd6a7cf5451505b060a90c19faed88ff33d80d4
Marina Sovic <msovic@itglue.com>
2017-09-28 20:27:25 -0700

    Use idxs instead of sizes

--- js/player.js
+++ js/player.js
@@ -1,7 +1,12 @@
 var imgSrc1 = "imgs/player1.png";
 var imgSrc2 = "imgs/player2.png";
 
-var frame = 1;
+var frame = 0;
+
+/*
+    x - indexed position of player
+    y - indexed position of player
+*/
 
 class Player {
     constructor (keysDown) {
@@ -12,43 +17,49 @@ class Player {
     }
 
     updateState() {
-        if (frame % 25) {
-            return;
+        if (!(frame % 25)) {
+            if (this.keysDown['left']){
+                this.x--;
+            }
+            if (this.keysDown['right']){
+                this.x++;
+            }
+            if (this.keysDown['up']){
+                this.y--;
+            }
+            if (this.keysDown['down']){
+                this.y++;
+            }
         }
 
-        if (this.keysDown['left']){
-            this.x += -app.size;
-	    }
-    	if (this.keysDown['right']){
-    		this.x += app.size;
-    	}
-    	if (this.keysDown['up']){
-    		this.y += -app.size;
-    	}
-    	if (this.keysDown['down']){
-    		this.y += app.size;
-    	}
+        this.limitWithinCanvas();
 
-	    if (this.x > app.canvasWidth) this.x = app.canvasWidth - app.size;
-        if (this.x < 0) this.x = 0;
+        this.sprite.updatePosition(this.x * app.size, this.y * app.size);
 
-        if (this.y > app.canvasHeight) this.y = app.canvasHeight - app.size;
-        if (this.y < 0) this.y = 0;
-
-        this.sprite.updatePosition(this.x, this.y);
-    }
-
-    draw () {
         frame++;
         if (!(frame % 30)) {
             this.sprite.setImageSrc(this.sprite.getImageSrc().includes(imgSrc1) ? imgSrc2 : imgSrc1);
         }
+    }
+
+    limitWithinCanvas () {
+        // check if player is within limitations of canvasWidt
+        if (this.x * app.size > app.canvasWidth - app.size) this.x--;
+        if (this.x * app.size < 0) this.x = 0;
+
+        if (this.y * app.size > app.canvasHeight - app.size) this.y--;
+        if (this.y * app.size < 0) this.y = 0;
+    }
+
+
+    draw () {
+        this.print();
         this.sprite.draw();
     }
 
     print () {
-        console.log("x: " + this.x);
-        console.log("y: " + this.y);
+        console.log("x: " + this.x*app.size);
+        console.log("y: " + this.y*app.size);
         console.log("  ");
     }
 
