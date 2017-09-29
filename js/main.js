b970506da08bf8d2e6ce38d015cbbedd712d8d67
Marina Sovic <msovic@itglue.com>
2017-09-28 17:32:47 -0700

    Start game

--- js/main.js
+++ js/main.js
@@ -3,10 +3,33 @@ let app = {
     canvasHeight: 700
 };
 
-function load (){
+function renderCanvas () {
     app.canvas  = document.getElementById("mainCanvas");
 	app.canvas.width = app.canvasWidth;
 	app.canvas.height = app.canvasHeight;
 	app.ctx = app.canvas.getContext('2d');
 	app.ctx.fillStyle = "white";
+}
+
+
+function updateState () {
+}
+
+function draw () {
+    renderCanvas();
+    renderMaze();
+}
+
+function renderMaze () {
+}
+
+function loop(timestamp) {
+    updateState()
+    draw();
+    requestAnimationFrame(loop)
+}
+
+
+function startGame () {
+    requestAnimationFrame(loop)
 }
